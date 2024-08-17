f46eaa9615eba84fe2464827a54082aff83732d4
quoctrung19 <trungpqse172561@fpt.edu.vn>
2024-08-17 23:48:06 +0700

    Learn ComponentDidMount

--- beginer/src/Clock.jsx
+++ beginer/src/Clock.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const lists = ['BMW', 'Toyota', 'Honda']
+
+const fetchApi = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(lists)
+    }, 1000)
+  })
+}
 export default class Clock extends React.Component {
   constructor(props) {
     super(props)
@@ -9,7 +18,8 @@ export default class Clock extends React.Component {
       },
       seconds: {
         created: new Date().getSeconds()
-      }
+      },
+      lists: []
     }
   }
 
@@ -24,12 +34,25 @@ export default class Clock extends React.Component {
     this.setState(newState)
   }
 
+  componentDidMount() {
+    const seconds = document.getElementById('seconds')
+    console.log(seconds)
+
+    fetchApi().then((res) =>
+      this.setState((prevState) => ({
+        ...prevState,
+        lists: res
+      }))
+    )
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>Hello World!</h1>
         <h2>It's {this.state.time.created}</h2>
-        <h2>It's {this.state.seconds.created}</h2>
+        <h2 id='seconds'>It's {this.state.seconds.created}</h2>
         <button onClick={this.getTime}>Get Time</button>
       </div>
     )

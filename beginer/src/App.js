5720527a0a4dc38ab1af85c7050aa6584dcde0d4
quoctrung19 <trungpqse172561@fpt.edu.vn>
2024-08-20 15:41:36 +0700

    Using children

--- beginer/src/App.js
+++ beginer/src/App.js
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import Clock from './Clock'
 import BareInput from './BareInput'
+import LayOut from './Layout'
 
 function App() {
   const [visible, setVisible] = useState(true)
 
   return (
-    // <div className='App'>
-    //   <button onClick={() => setVisible(false)}>Hide Clock component</button>
-    //   {visible && <Clock />}
-    // </div>
-
-    <BareInput type='text' value='100' autoFocus /> //missing onChange when using functional. will learn later
+    <div className='App'>
+      {/* <button onClick={() => setVisible(false)}>Hide Clock component</button>
+       {visible && <Clock />} */}
+      <LayOut>
+        <h1>This is layout</h1>
+      </LayOut>
+      <BareInput type='text' value='100' autoFocus /> {/* //missing onChange when using functional. will learn later */}
+    </div>
   )
 }
 
---
+++ beginer/src/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// class LayOut extends React.Component {
+//   render() {
+//     return <div className='layout'>{this.props.children}</div>
+//   }
+// }
+
+function LayOut(children) {
+  return <div className='layout'>{children}</div>
+}
+export default LayOut

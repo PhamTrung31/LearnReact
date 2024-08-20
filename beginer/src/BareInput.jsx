ae50e06c9efce5844b028320c8e535d86e96fca3
quoctrung19 <trungpqse172561@fpt.edu.vn>
2024-08-20 15:12:50 +0700

    Change name when using props

--- beginer/src/BareInput.jsx
+++ beginer/src/BareInput.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-// class BareInput extends React.Component {
-//   render() {
-//     const { type, ...rest } = this.props
-//     return <input {...rest} />
-//   }
-// }
-
-function BareInput({ type, ...rest }) {
-  return <input {...rest} />
+class BareInput extends React.Component {
+  render() {
+    const type = 100
+    const { type: typeInput, ...rest } = this.props //avoid error
+    return <input {...rest} type={typeInput} />
+  }
 }
+
+// function BareInput({ type, ...rest }) {
+//   return <input {...rest} />
+// }
 
 export default BareInput

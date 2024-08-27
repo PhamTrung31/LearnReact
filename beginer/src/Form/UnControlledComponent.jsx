2129c7374220eaaaaf50ce149d15d86ebfff8a5e
quoctrung19 <trungpqse172561@fpt.edu.vn>
2024-08-27 17:04:43 +0700

    UnControlledComponent

---
+++ beginer/src/Form/UnControlledComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class UnControlledComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      first
+    }
+  }
+  render() {
+    return <div>UnControlledComponent</div>
+  }
+}
+
+export default UnControlledComponent
